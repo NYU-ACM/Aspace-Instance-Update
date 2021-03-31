d2f19d651542e2fe293c2b86242ba522f20eb426
Don Mennerich <don.mennerich@nyu.edu>
2021-03-31 11:23:35 -0400

    getting topcontainer map

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/nyudlts/go-aspace"
 	"os"
 	"strings"
 )
@@ -21,8 +22,10 @@ type Row struct {
 }
 
 func main() {
+	fmt.Println("aspace-instance-update")
 	//open a work order and check for errors
-	tsv, err := os.Open("test.tsv")
+	fmt.Println("1. Parsing work order")
+	tsv, err := os.Open(os.Args[1])
 	if err != nil {
 		panic(err)
 	}
@@ -32,9 +35,39 @@ func main() {
 		panic(err)
 	}
 
-	//print each row in the Array
+	fmt.Println("2. Getting Aspace client")
+	//get a go-aspace client
+	client, err := aspace.NewClient("dev", 20)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("3. Getting repository and resource IDs")
+	//get the repository ID from the first row of the TSV
+	repositoryId,aoID, err := aspace.URISplit(rows[1].URI)
+	if err != nil {
+		panic(err)
+	}
+
+	//Get the resource ID from the first row of the TSV -- this is a hack
+	ao, err := client.GetArchivalObject(repositoryId, aoID)
+	if err != nil {
+		panic(err)
+	}
+	_, resourceId, err := aspace.URISplit(ao.Resource["ref"])
+
+	//Get a map of Top Containers from aspace  for the resource
+	fmt.Println("4. Getting Top Containers for resource")
+	topContainers, err := client.GetTopContainersForResource(repositoryId, resourceId)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("5. Updating AO indicators and Top Container URI")
+	//iterate each row in the Array
 	for _, row := range rows {
-		fmt.Println(row)
+		tc := topContainers[row.ContainerIndicator1]
+		fmt.Println(tc.Indicator, "->", tc.Barcode)  //the function to test,update,and undo topcontainer info goes here.
 	}
 }
 
